3f2b27d35574450e74f242e216c0c253eb90f13c
Fahad.Hassan <fahadfuuast2@gmail.com>
2024-05-04 00:19:26 +0500

    updated

---
+++ Local-Storage/app.js
@@ -0,0 +1,14 @@
+// let login = document.getElementById("login");
+// let signup = document.getElementById("signup");
+// let log = document.querySelector(".logins")
+// log.style.display = "none";
+
+// function signup() {
+
+// let user = document.getElementById("user").value
+// let sigemail = document.getElementById("sigemail").value
+// let sigpass = document.getElementById("signpass").value
+// localStorage.setItem("users", JSON.stringify({userName: user, sign:sigemail, pass:sigpass}))
+
+
+// }
---
+++ Local-Storage/script.js
@@ -0,0 +1,11 @@
+let login = document.getElementById("login");
+let signup = document.getElementById("signup");
+let log = document.querySelector(".logins")
+log.style.display = "none";
+
+function signup() {
+
+let user = document.getElementById()
+
+
+}
